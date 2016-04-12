77d7564a5877b5f1712e2fd3ac1dc2b5d3b37ec8
Tan <thnguyn2@illinois.edu>
2016-04-12 10:56:39 -0500

    Add code to decimating in z from 63x to 40x

--- axial_decimation_40x_from_63x.m
+++ axial_decimation_40x_from_63x.m
@@ -3,8 +3,8 @@ clc;
 close all;
 clear all;
 Ma = 0.6349; %Downsampling factor in xy
-curfolder = 'E:\Tan_QDIC_hela_63x\';
-outdir = 'E:\Tan_QDIC_hela_40x\';
+curfolder = 'H:\Tan_QDIC_hela_63x\';
+outdir = 'H:\Tan_QDIC_hela_40x\';
 if (~exist(outdir))
     mkdir(outdir);
 end
@@ -28,7 +28,7 @@ end
 %p=gcp;
 %delete(p);
 %p=parpool(8);
-override = 1;
+override = 0;
 frametype = 'qdic';
 if (strcmp(frametype,'raw'))
     ss = {'m0','m1','m2','m3'}; 
